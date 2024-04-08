d08fe44e52ce0fa739f7b1d69905f54344c23b5c
vkbati <118110811+vkbati@users.noreply.github.com>
2024-04-08 02:36:40 +0200

    Bati

--- manuel_tests/manuel_test_query.py
+++ manuel_tests/manuel_test_query.py
@@ -1,5 +1,5 @@
 import news_search
-api_key = "a82492b6f44d45a685e1712f3e644552"
+api_key = "YOUR API"
 
 eng_query = news_search.query("bitcoin", api_key=api_key, lang="en", max_news=1)
 ger_query = news_search.query("bitcoin", api_key=api_key, lang="de", max_news=2)
